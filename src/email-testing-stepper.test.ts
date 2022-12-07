11b391aeb65b3f8c14cf678e8021a767f1159bdc
Vid <vid_github@zooid.org>
2022-12-07 10:30:10 -0500

    fix test

--- src/email-testing-stepper.test.ts
+++ src/email-testing-stepper.test.ts
@@ -1,27 +1,33 @@
-import { testWithDefaults } from '@haibun/core/build/lib/test/lib';
+import { asFeatures, testWithDefaults } from '@haibun/core/build/lib/test/lib';
 import { getStepperOptionName } from '@haibun/core/build/lib/util';
-import { DEF_PROTO_DEFAULT_OPTIONS } from '@haibun/core/build/lib/run';
+import { DEF_PROTO_OPTIONS } from '@haibun/core/build/lib/run';
 import EmailTestingStepper from './email-testing-stepper';
 
 const domain = 'google.com';
 
+const protoOptions = {
+  ...DEF_PROTO_OPTIONS,
+  extraOptions: {
+    [getStepperOptionName(EmailTestingStepper, 'EMAIL_SERVER')]: domain,
+  }
+}
 describe('MTA', () => {
   it('MTA STS', async () => {
-    const protoOptions = { extraOptions: { [getStepperOptionName(EmailTestingStepper, 'EMAIL_SERVER')]: domain, }, options: DEF_PROTO_DEFAULT_OPTIONS }
-    const { ok } = await testWithDefaults(`domain's mail server agent strict transfer security is valid`, [EmailTestingStepper], protoOptions);
+    const features = asFeatures([{ path: '/features/record.feature', content: `domain's mail server agent strict transfer security is valid` }]);
+    const { ok } = await testWithDefaults(features, [EmailTestingStepper], protoOptions);
     expect(ok).toBe(true);
   });
 });
 
 describe('DMARC', () => {
   it('Has a valid dmarc record', async () => {
-    const protoOptions = { extraOptions: { [getStepperOptionName(EmailTestingStepper, 'EMAIL_SERVER')]: domain, }, options: DEF_PROTO_DEFAULT_OPTIONS }
-    const { ok } = await testWithDefaults(`DMARC record exists`, [EmailTestingStepper], protoOptions);
+    const features = asFeatures([{ path: '/features/record.feature', content: `DMARC record exists` }]);
+    const { ok } = await testWithDefaults(features, [EmailTestingStepper], protoOptions);
     expect(ok).toBe(true);
   });
   it('DMARC rua is defined', async () => {
-    const protoOptions = { extraOptions: { [getStepperOptionName(EmailTestingStepper, 'EMAIL_SERVER')]: domain, }, options: DEF_PROTO_DEFAULT_OPTIONS }
-    const { ok } = await testWithDefaults(`DMARC field rua is defined`, [EmailTestingStepper], protoOptions);
+    const features = asFeatures([{ path: '/features/record.feature', content: `DMARC field rua is defined` }]);
+    const { ok } = await testWithDefaults(features, [EmailTestingStepper], protoOptions);
     expect(ok).toBe(true);
   });
 });
