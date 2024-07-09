8af04d9a2c452664efd927139c064f45bd0885d4
vid <vid_github@zooid.org>
2024-07-09 10:27:48 -0400

    update and add parameter for domain

--- src/email-testing-stepper.test.ts
+++ src/email-testing-stepper.test.ts
@@ -1,7 +1,11 @@
-import { asFeatures, testWithDefaults } from '@haibun/core/build/lib/test/lib';
+import { describe, it, expect } from 'vitest';
+
+import { testWithDefaults } from '@haibun/core/build/lib/test/lib.js';
+import { asFeatures } from '@haibun/core/build/lib/resolver-features.js';
+
 import { getStepperOptionName } from '@haibun/core/build/lib/util';
-import { DEF_PROTO_OPTIONS } from '@haibun/core/build/lib/run';
-import EmailTestingStepper from './email-testing-stepper';
+import { DEF_PROTO_OPTIONS } from '@haibun/core/build/lib/run.js';
+import EmailTestingStepper from './email-testing-stepper.js';
 
 const domain = 'google.com';
 
@@ -9,25 +13,49 @@ const protoOptions = {
   ...DEF_PROTO_OPTIONS,
   extraOptions: {
     [getStepperOptionName(EmailTestingStepper, 'EMAIL_SERVER')]: domain,
-  }
-}
+  },
+};
 describe('MTA', () => {
   it('MTA STS', async () => {
-    const features = asFeatures([{ path: '/features/record.feature', content: `domain's mail server agent strict transfer security is valid` }]);
-    const { ok } = await testWithDefaults(features, [EmailTestingStepper], protoOptions);
+    const features = asFeatures([
+      {
+        path: '/features/record.feature',
+        content: `domain's mail server agent strict transfer security is valid`,
+      },
+    ]);
+    const { ok } = await testWithDefaults(
+      features,
+      [EmailTestingStepper],
+      protoOptions,
+    );
     expect(ok).toBe(true);
   });
 });
 
 describe('DMARC', () => {
   it('Has a valid dmarc record', async () => {
-    const features = asFeatures([{ path: '/features/record.feature', content: `DMARC record exists` }]);
-    const { ok } = await testWithDefaults(features, [EmailTestingStepper], protoOptions);
+    const features = asFeatures([
+      { path: '/features/record.feature', content: `DMARC record exists` },
+    ]);
+    const { ok } = await testWithDefaults(
+      features,
+      [EmailTestingStepper],
+      protoOptions,
+    );
     expect(ok).toBe(true);
   });
   it('DMARC rua is defined', async () => {
-    const features = asFeatures([{ path: '/features/record.feature', content: `DMARC field rua is defined` }]);
-    const { ok } = await testWithDefaults(features, [EmailTestingStepper], protoOptions);
+    const features = asFeatures([
+      {
+        path: '/features/record.feature',
+        content: `DMARC field rua is defined`,
+      },
+    ]);
+    const { ok } = await testWithDefaults(
+      features,
+      [EmailTestingStepper],
+      protoOptions,
+    );
     expect(ok).toBe(true);
   });
 });
--- src/email-testing-stepper.ts
+++ src/email-testing-stepper.ts
@@ -1,136 +1,151 @@
-
 const { authenticate } = require('mailauth');
 const { dkimVerify } = require('mailauth/lib/dkim/verify');
 const { spf } = require('mailauth/lib/spf');
 const { getPolicy, validateMx } = require('mailauth/lib/mta-sts');
 const dmarc = require('dmarc-solution');
 
-
-import { AStepper, IHasOptions, OK, TOptionValue, TWorld } from "@haibun/core/build/lib/defs";
+import { AStepper, IHasOptions, OK, TNamed, TWorld } from '@haibun/core/build/lib/defs';
 import { actionNotOK, getStepperOption, stringOrError } from '@haibun/core/build/lib/util';
 
 const EMAIL_SERVER = 'EMAIL_SERVER';
 const EMAIL_MESSAGE = 'EMAIL_MESSAGE';
 
 const EmailTestingStepper = class EmailTestingStepper extends AStepper implements IHasOptions {
-    knownPolicy = undefined;
-    emailServer: undefined | string;
-    // requireDomains = [EMAIL_SERVER, EMAIL_MESSAGE];
-    options = {
-        [EMAIL_SERVER]: {
-            desc: 'Target email server',
-            parse: (input: string) => stringOrError(input)
-        },
-    };
-    async setWorld(world: TWorld, steppers: AStepper[]) {
-        super.setWorld(world, steppers);
-        this.world = world;
-        this.emailServer = getStepperOption(this, EMAIL_SERVER, this.world.extraOptions);
-    }
-    async getPolicy() {
-        const res = await getPolicy(this.emailServer, this.knownPolicy);
-        return res;
-    }
+  knownPolicy = undefined;
+  emailServer: undefined | string;
+  // requireDomains = [EMAIL_SERVER, EMAIL_MESSAGE];
+  options = {
+    [EMAIL_SERVER]: {
+      desc: 'Target email server',
+      parse: (input: string) => stringOrError(input),
+    },
+  };
+  async setWorld(world: TWorld, steppers: AStepper[]) {
+    super.setWorld(world, steppers);
+    this.world = world;
+    this.emailServer = getStepperOption(this, EMAIL_SERVER, this.world.extraOptions);
+  }
+  async getPolicy() {
+    const res = await getPolicy(this.emailServer, this.knownPolicy);
+    return res;
+  }
 
-    steps = {
-        dmarcExists: {
-            gwta: `DMARC record exists`,
-            action: async () => {
-                try {
-                    const record = await dmarc.fetch(this.emailServer);
-                    return OK;
-                } catch (e: any) {
-                    return actionNotOK(e.getMessage());
-                }
-            }
-        },
-        dmarcHas: {
-            gwta: `DMARC field {field} is defined`,
-            action: async ({ field }: TOptionValue) => {
-                try {
-                    const record = await dmarc.fetch(this.emailServer);
-                    if (!record.tags[field]) {
-                        return actionNotOK(`field ${field} doesn't exist`);
-                    }
-                    return OK;
-                } catch (e: any) {
-                    return actionNotOK(e.getMessage());
-                }
-            }
-        },
-        checkMTASTS: {
-            gwta: `domain's mail server agent strict transfer security is valid`,
-            action: async () => {
-                const { policy } = await this.getPolicy();
+  steps = {
+    dmarcExists: {
+      gwta: `DMARC record exists`,
+      action: async () => {
+        return this.checkDmarc(this.emailServer);
+      },
+    },
+    dmarcExistsFor: {
+      gwta: `DMARC record exists for {domain}`,
+      action: async ({ domain }: TNamed) => {
+        return await this.checkDmarc(domain);
+      },
+    },
+    dmarcHas: {
+      gwta: `DMARC field {field} is defined`,
+      action: async ({ field }: TNamed) => {
+        return await this.checkDmarcField(this.emailServer, field);
+      },
+    },
+    dmarcHasFor: {
+      gwta: `DMARC field {field} is defined for {domain}`,
+      action: async ({ domain, field }: TNamed) => {
+        return await this.checkDmarcField(this.emailServer, field);
+      },
+    },
+    checkMTASTS: {
+      gwta: `domain's mail server agent strict transfer security is valid`,
+      action: async () => {
+        const { policy } = await this.getPolicy();
 
-                for (const mx of policy.mx) {
-                    const policyMatch = validateMx(mx, policy);
-                    if (!policy.id) {
-                        return actionNotOK(policyMatch);
-                    }
-                    if (policy.mx && !policyMatch) {
-                        return actionNotOK(`unlisted MX ${mx}`);
-                    }
-                }
-
-                if (policy.mode !== 'enforce') {
-                    return actionNotOK(`not using enforce mode`);
-                }
-
-                return OK;
-            }
-        },
-        validateMXHostname: {
-            gwta: `domain has a valid mail exchange record`,
-            action: async () => {
-                const { policy } = await this.getPolicy();
-                const res = await validateMx(this.emailServer, policy);
-
-                return res === true ? OK : actionNotOK(res);
-            }
-        },
-        authenticate: {
-            gwta: `authenticate`,
-            action: async () => {
-                const message = 'hello';
-                const { dkim, spf, arc, dmarc, bimi, receivedChain, headers } = await authenticate(
-                    message, // either a String, a Buffer or a Readable Stream
-                    {
-                        // SMTP transmission options if available
-                        ip: '217.146.67.33', // SMTP client IP
-                        helo: 'uvn-67-33.tll01.zonevs.eu', // EHLO/HELO hostname
-                        sender: 'andris@ekiri.ee', // MAIL FROM address
-
-                        // Uncomment if you do not want to provide ip/helo/sender manually but parse from the message
-                        //trustReceived: true,
-
-                        // Server processing this message, defaults to os.hostname(). Inserted into Authentication headers
-                        mta: 'mx.ethereal.email',
-
-                        //  Optional  DNS resolver function (defaults to `dns.promises.resolve`)
-                        // resolver: async (name: any, rr: any) => await dns.promises.resolve(name, rr)
-                    }
-                );
-                // output authenticated message
-                process.stdout.write(headers); // includes terminating line break
-                process.stdout.write(message);
-                return OK;
-            }
-        },
-        spf: {
-            gwta: `spf`,
-            action: async () => {
-
-                let result = await spf({
-                    sender: 'andris@wildduck.email',
-                    ip: '217.146.76.20',
-                    helo: 'foo',
-                    mta: 'mx.myhost.com'
-                });
-                return OK;
-            }
+        for (const mx of policy.mx) {
+          const policyMatch = validateMx(mx, policy);
+          if (!policy.id) {
+            return actionNotOK(policyMatch);
+          }
+          if (policy.mx && !policyMatch) {
+            return actionNotOK(`unlisted MX ${mx}`);
+          }
         }
+
+        if (policy.mode !== 'enforce') {
+          return actionNotOK(`not using enforce mode`);
+        }
+
+        return OK;
+      },
+    },
+    validateMXHostname: {
+      gwta: `domain has a valid mail exchange record`,
+      action: async () => {
+        const { policy } = await this.getPolicy();
+        const res = await validateMx(this.emailServer, policy);
+
+        return res === true ? OK : actionNotOK(res);
+      },
+    },
+    authenticate: {
+      gwta: `authenticate`,
+      action: async () => {
+        const message = 'hello';
+        const { dkim, spf, arc, dmarc, bimi, receivedChain, headers } = await authenticate(
+          message, // either a String, a Buffer or a Readable Stream
+          {
+            // SMTP transmission options if available
+            ip: '217.146.67.33', // SMTP client IP
+            helo: 'uvn-67-33.tll01.zonevs.eu', // EHLO/HELO hostname
+            sender: 'andris@ekiri.ee', // MAIL FROM address
+
+            // Uncomment if you do not want to provide ip/helo/sender manually but parse from the message
+            //trustReceived: true,
+
+            // Server processing this message, defaults to os.hostname(). Inserted into Authentication headers
+            mta: 'mx.ethereal.email',
+
+            //  Optional  DNS resolver function (defaults to `dns.promises.resolve`)
+            // resolver: async (name: any, rr: any) => await dns.promises.resolve(name, rr)
+          },
+        );
+        // output authenticated message
+        process.stdout.write(headers); // includes terminating line break
+        process.stdout.write(message);
+        return OK;
+      },
+    },
+    spf: {
+      gwta: `spf`,
+      action: async () => {
+        let result = await spf({
+          sender: 'andris@wildduck.email',
+          ip: '217.146.76.20',
+          helo: 'foo',
+          mta: 'mx.myhost.com',
+        });
+        return OK;
+      },
+    },
+  };
+  async checkDmarc(emailServer: string) {
+    try {
+      const record = await dmarc.fetch(this.emailServer);
+      return OK;
+    } catch (e: any) {
+      return actionNotOK(e.getMessage());
     }
-}
+  }
+  async checkDmarcField(emailServer: string, field: any) {
+    try {
+      const record = await dmarc.fetch(emailServer);
+      if (!record.tags[field]) {
+        return actionNotOK(`field ${field} doesn't exist`);
+      }
+      return OK;
+    } catch (e: any) {
+      return actionNotOK(e.getMessage());
+    }
+  }
+};
 
 export default EmailTestingStepper;
